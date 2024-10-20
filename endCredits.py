dd70a61ab3d3fd9bcec5ce3f187b96d0e8884f38
jeslyng <143136280+jeslyng@users.noreply.github.com>
2024-10-20 11:54:25 -0500

    Update endCredits.py

--- endCredits.py
+++ endCredits.py
@@ -1,8 +1,7 @@
 class EndCredits(Scene):
     def construct(self):
-        text1 = Text("We used Manim in this project")
-        text2 = Text("which is an open-source Python")
-        text3 = Text("library created by 3Blue1Brown.")
-        group = VGroup(text1, text2, text3).arrange(DOWN,buff = 0.2).shift(UP)
+        text1 = Text("Credits:")
+        text2 = Text("Manim and TripShot")
+        group = VGroup(text1, text2).arrange(DOWN,buff = 0.2).shift(UP)
         self.play(Write(group), run_time=5)
         self.wait(2)
